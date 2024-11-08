b5656fe117b308bca7634ac3e6dfd7524c8e30a3
guklein <gustavokleing@gmail.com>
2024-11-07 21:44:13 -0300

    ajuste inicial e teste para cadastro de cliente

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,18 +1,24 @@
+<header>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</header>
 <a href="{{route('clientes.create')}}" class="btn btn-primary">Novo Cliente</a>
 
-<table class="table">
+<table class="table table-striped">
+    
     <thead>
         <tr>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Telefone</th>
-            <th>Endereço</th>
-            <th>Cpf</th>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Endereço</th>
+            <th scope="col">Cpf</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($clientes as $cliente)
+        
             <tr>
                 <td class="colunas">{{$cliente->id}}</td>
                 <td id="nome">{{$cliente->nome}}</td>
